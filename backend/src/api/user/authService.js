b8670557742dab134467ed3d80d205ab1fa471e2
Carlos Joaquim <carlos.joaquim@dextra-sw.com>
2019-03-27 15:10:24 -0300

    Implementando signup

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -13,3 +13,50 @@ const sendErrosFromDb = (res, dbErros) => {
     _forIn(dbErros.errors, error => errors.push(error.message))
     return res.status(400).json({errors})
 }
+
+const login = (req, res, next) => {
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+
+    User.findOne({email}, (err, user) => {
+        if(err){
+            return sendErrosFromDb(res, err)
+        } else if (user && bcrypt.compareSync(password, user.password)){
+            const token = jwt.sign(user, env.authSecret, {
+                expiresIn = "1h"
+            })
+            const {name, email} = user
+            res.json({name, email}, token)
+        } else {
+            return res.status(400).send({erros: ['Usuário/Senha inválidos!']})
+        }
+    })
+}
+
+const validateToken = (req, res, next) => {
+    const token = req.body.token || ''
+    jwt.verify(token, env.authSecret, function(err, decoded){
+        return res.status(200).send({valid: !err})
+    })
+}
+
+const signup = (req, res, next) => {
+    const email = req.body.email || ''
+    const name = req.body.name || ''
+    const password = req.body.password || ''
+    const confirmPassword = req.bod.confirm_password || ''
+
+    if(!email.match(emailRegex)){
+        return res.status(400).send({errors: ['O e-mail informado é inválido!']})
+    }
+
+    if(!password.match(passwordRegex)){
+        return res.status(400).send({
+            errors: [
+                "Senha precisar ter: uma letra maiúscula, uma letra minúscula, um número, uma caractere especial(@#$%) e tamanho entre 6-20."
+            ]
+        })
+    }
+
+    
+}
